949b792f8dfe949dbf39b6af84474021dca1b200
nathan <nathan.guardado@yahoo.com>
2023-10-29 19:24:51 -0400

    Finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,6 +5,14 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
+        Person person1 = new Person("Harry", 30, 180);
+
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info(String.valueOf(person1.height));
+
+        person1.height = 200;
+        logger.info(String.valueOf(person1.height));
+
+        Car[] cars = new Car[3];
+    }
 }
---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,15 @@
+package object1;
+
+public class Person {
+        String name;
+        int age = 0;
+        //units are in centimeters
+        int height = 0;
+
+        // Constructor
+        public Person(String name, int age, int height) {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+        }
+}
